62fd7415e6f8325708bc906fc789a5132944976e
Takym <15681312+Takym@users.noreply.github.com>
2022-05-25 14:07:14 +0900

    ファイルを削除

--- BinFuck/bfk.cmd
+++
@@ -1,6 +0,0 @@
-@REM The BinFuck Interpreter
-@REM Copyright (C) 2020-2022 Takym.
-@REM distributed under the MIT License.
-@echo off
-
-call csi.exe BinFuck.csx %*
---
+++ README.md
@@ -0,0 +1,4 @@
+# The BinFuck Interpreter
+Copyright (C) 2020-2022 Takym.
+
+* このリポジトリは [Gradexor](https://github.com/Takym/Gradexor/tree/master/BinFuck) へ移動しました。
